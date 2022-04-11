0cbeaabdd1bd74f18086fda34363d1ca0e907001
Martín Montes <martin11lrx@gmail.com>
2022-04-11 18:45:24 +0200

    Added benchmarks for tree

--- go.mod
+++ go.mod
@@ -10,7 +10,11 @@ require (
 )
 
 require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/google/pprof v0.0.0-20220401020641-b5a4dc8f4f2a // indirect
+	github.com/ianlancetaylor/demangle v0.0.0-20210905161508-09a460cdf81d // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- tree/tree_test.go
+++ tree/tree_test.go
@@ -2,6 +2,7 @@ package tree_test
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -58,6 +59,36 @@ func TestTree_FindNotFound(t *testing.T) {
 	require.Nil(t, value)
 }
 
+var sizes = []int{1e2, 1e3, 1e4}
+
+func BenchmarkInsert(b *testing.B) {
+	for _, n := range sizes {
+		b.Run(fmt.Sprintf("BenchmarkInsert_%d", n), func(b *testing.B) {
+			s := generateRandomSlice(n)
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				getTree(s)
+			}
+		})
+	}
+}
+
+func BenchmarkFind(b *testing.B) {
+	for _, n := range sizes {
+		b.Run(fmt.Sprintf("BenchmarkFind_%d", n), func(b *testing.B) {
+			s := generateRandomSlice(n)
+			tr := getTree(s)
+			key := randRange(0, n)
+			b.ResetTimer()
+
+			for i := 0; i < b.N; i++ {
+				tr.Find(key)
+			}
+		})
+	}
+}
+
 func compareInt(keyA, keyB interface{}) int {
 	if keyA == keyB {
 		return 0
@@ -77,4 +108,24 @@ func compareInt(keyA, keyB interface{}) int {
 		return 0
 	}
 	return 1
+}
+
+func generateRandomSlice(n int) []int {
+	s := make([]int, n)
+	for i := 0; i < n; i++ {
+		s[i] = randRange(0, n)
+	}
+	return s
+}
+
+func getTree(s []int) *tree.Tree {
+	tr := tree.New(compareInt)
+	for k, v := range s {
+		tr.Insert(k, v)
+	}
+	return tr
+}
+
+func randRange(min, max int) int {
+	return rand.Intn(max+1-min) + min
 }
